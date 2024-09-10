ab8ee64ef3842b8f737d941cc45235cca2215419
yblir <1213401388@qq.com>
2024-09-10 17:27:25 +0800

    b3716

---
+++ common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 0;
+char const *LLAMA_COMMIT = "unknown";
+char const *LLAMA_COMPILER = "cc (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
