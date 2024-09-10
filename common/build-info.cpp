24675a74d1462060be65364b1d01a67ae7dda3c4
yblir <1213401388@qq.com>
2024-09-10 17:41:40 +0800

    增加文件llama.cpp流程2

--- common/build-info.cpp
+++ common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 0;
-char const *LLAMA_COMMIT = "unknown";
+int LLAMA_BUILD_NUMBER = 1;
+char const *LLAMA_COMMIT = "ab8ee64";
 char const *LLAMA_COMPILER = "cc (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0";
 char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
